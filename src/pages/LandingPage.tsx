95ba3813d00f6ce155f47f434a7cafaec5abe8d4
eunbin58 <98297421+eunbin58@users.noreply.github.com>
2025-12-19 22:24:22 +0900

    Feature/AS-77 (#23)
    * 테스트 페이지 삭제
    
    * 메인 페이지 이미지
    
    * 사이드바 변경
    
    * 안내메시지 변경

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -36,16 +36,26 @@ export default function LandingPage() {
 
             {/* popover */}
             <div
-              className={`absolute right-0 top-11 w-[280px] rounded-xl bg-[#F7F1E6] p-4 text-sm text-neutral-900 shadow-lg transition-all
+               className={`absolute right-0 top-11 w-[280px] rounded-xl bg-[#F7F1E6] p-4 text-base text-neutral-900 shadow-lg transition-all
               ${openHelp ? "opacity-100 translate-y-0" : "pointer-events-none opacity-0 -translate-y-1"}`}
             >
-              <div className="font-semibold">사용법</div>
-              <ul className="mt-2 list-disc space-y-1 pl-5 leading-5">
-                <li>먼저 로그인하고 나만의 창문을 만들어요.</li>
-                <li>전구를 눌러 편지를 확인/관리할 수 있어요.</li>
-                <li>로그인 후 우측 상단 메뉴(≡)에서 설정/내 창문을 이동해요.</li>
-              </ul>
-              <div className="mt-3 text-xs text-neutral-600">
+              <div className="font-semibold">✍️ 편지 작성 방법</div>
+              <ol className="mt-2 list-decimal space-y-1 pl-5 leading-5">
+                <li>편지를 남길 지인의 창문으로 이동해 줘~</li>
+                <li>창문을 밝혀줄 전구와 색상을 선택해 봐!</li>
+                <li>따뜻한 메시지로 마음을 전달해 보자~!</li>
+              </ol>
+              <p>💌 로그인하지 않아도 편지 남길 수 있어! <span className="text-[#006F57] font-medium">(❁´◡`❁)</span></p>
+              <br />
+
+              <div className="font-semibold">🪟 창문 생성 방법 (회원가입)</div>
+              <ol className="mt-2 list-decimal space-y-1 pl-5 leading-5">
+                <li>메인 페이지에서 “로그인하기” 버튼 클릭!</li>
+                <li>지인의 창문 페이지에서 “오른쪽 위에 사람” 버튼 클릭!</li>
+              </ol>
+               <p>💌 작성한 메시지는 12월 25일에 공개될 거야~! <span className="text-[#006F57] font-medium">(ღˇᴗˇ)｡o♡</span></p>
+              
+               <div className="mt-3 text-xs text-neutral-600">
                 (모바일에서는 i 버튼을 한 번 더 누르면 닫혀요)
               </div>
             </div>
