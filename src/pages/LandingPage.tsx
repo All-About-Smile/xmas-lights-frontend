b1912addf2e51f4c3b59abf47425ba5d12a04c1a
Lacoruna <jyp0430@naver.com>
2025-12-21 19:05:23 +0900

    창문 이모지 깨져서 다른 걸로 대체

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -51,7 +51,7 @@ export default function LandingPage() {
               <p>💌 로그인하지 않아도 편지 남길 수 있어! <div className="text-[#006F57] font-medium">(❁´◡`❁)</div></p>
               <br />
 
-              <div className="font-semibold">🪟 창문 생성 방법 (회원가입)</div>
+              <div className="font-semibold">✨창문 생성 방법 (회원가입)</div>
               <ol className="mt-2 list-decimal space-y-1 pl-5 leading-5">
                 <li>메인 페이지에서 “로그인하기” 버튼 클릭! 또는</li>
                 <li>지인의 창문 페이지에서 “홈” 버튼 클릭!</li>
